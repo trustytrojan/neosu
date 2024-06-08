133c18937c3a851d4cad89a1100205201003a62d
kiwec <kiwec@kiwec.net>
2024-06-08 10:17:42 +0200

    Build for x64 on Windows

--- libraries/sdl/include/SDL_blendmode.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:77ff01d222e1e1a15ea1a3c416a4380bac782129432422abc9b68ab870891cbe
-size 4993
--- libraries/sdl/include/SDL_log.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8f8177e23be035311c1d04283db041ba08b844a40b84633ec789dd5ddb437341
-size 6491
