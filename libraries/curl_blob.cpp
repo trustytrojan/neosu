157a787e828c63971b0b9ef0dc01612419ba1f23
William Horvath <william@horvath.blog>
2025-08-07 14:39:31 -0700

    reduce curl_blob bloat

--- libraries/curl_blob.cpp
+++ libraries/curl_blob.cpp
@@ -1,4 +1,9 @@
-#if !defined(_MSC_VER) && !((defined(__cpp_pp_embed) && __cpp_pp_embed >= 202502L) || (defined(__clang__) && __clang_major__ >= 19))
+#if !defined(_MSC_VER)
+
+#include "curl_blob.h"
+
+// clang++ for some reason doesn't define this feature test macro but supports it anyways... (past ver 19)
+#if !((defined(__cpp_pp_embed) && __cpp_pp_embed >= 202502L) || (defined(__clang__) && __clang_major__ >= 19))
 
 asm(".section .rodata\n"
     ".balign 1\n"
@@ -11,4 +16,19 @@ asm(".section .rodata\n"
     "curl_ca_embed_end:\n"
     ".size curl_ca_embed, curl_ca_embed_end - curl_ca_embed\n");
 
+unsigned long long curl_ca_embed_size() { return &curl_ca_embed_end[0] - &curl_ca_embed[0]; }
+#else
+
+// This file is just an SSL CA certificate bundle, which we use to make secure requests with curl
+// (with CURLOPT_CAINFO_BLOB), without needing to rely on this data being found by curl/OpenSSL on the host
+constexpr const unsigned char curl_ca_embed[] = {
+// should be cached in <top-level>/build-aux/cache/curl-X.Y.Z/certbundle/cacert.pem
+#embed CURL_CERT_BUNDLE_PATH
+};
+
+constexpr const unsigned char curl_ca_embed_end[]{};
+
+unsigned long long curl_ca_embed_size() { return sizeof(curl_ca_embed); }
+
+#endif
 #endif
--- libraries/curl_blob.h
+++ libraries/curl_blob.h
@@ -2,25 +2,14 @@
 
 // unsupported with MSVC, unneeded anyways because it uses schannel instead of OpenSSL
 #ifndef _MSC_VER
-// clang++ for some reason doesn't define this feature test macro but supports it anyways... (past ver 19)
-#if (defined(__cpp_pp_embed) && __cpp_pp_embed >= 202502L) || (defined(__clang__) && __clang_major__ >= 19)
-// This file is just an SSL CA certificate bundle, which we use to make secure requests with curl
-// (with CURLOPT_CAINFO_BLOB), without needing to rely on this data being found by curl/OpenSSL on the host
-static constexpr const unsigned char curl_ca_embed[] = {
-#embed CURL_CERT_BUNDLE_PATH
-};
 
-static constexpr unsigned long long curl_ca_embed_size() { return sizeof(curl_ca_embed); }
-#else
 // data in curl_blob.c
 extern "C" {
 extern const unsigned char curl_ca_embed[];
 extern const unsigned char curl_ca_embed_end[];
 }
 
-static inline unsigned long long curl_ca_embed_size() { return &curl_ca_embed_end[0] - &curl_ca_embed[0]; }
-
-#endif
+unsigned long long curl_ca_embed_size();
 
 #define curl_easy_setopt_CAINFO_BLOB_embedded(curl) \
     struct curl_blob blob{};                        \
