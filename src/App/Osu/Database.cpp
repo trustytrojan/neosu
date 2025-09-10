trustytrojan/neosu

commit 81bc8355eeff0fa7c054e959a0e42508493d3827

Implement arbitrary database import
Closes #5

src/App/Osu/Database.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "ByteBufferedFile.h"
 #include "LegacyReplay.h"
 #include "Overrides.h"
 #include "UString.h"
@@ -13,7 +14,6 @@ class Timer;
 }
 class ConVar;
 
-class OsuFile;
 class DatabaseBeatmap;
 class DatabaseLoader;
 typedef DatabaseBeatmap BeatmapDifficulty;
@@ -93,7 +93,9 @@ class Database {
     DatabaseBeatmap *getBeatmapSet(i32 set_id);
 
     inline std::unordered_map<MD5Hash, std::vector<FinishedScore>> *getScores() { return &this->scores; }
-    inline const std::array<SCORE_SORTING_METHOD, 6> &getScoreSortingMethods() const { return this->scoreSortingMethods; }
+    inline const std::array<SCORE_SORTING_METHOD, 6> &getScoreSortingMethods() const {
+        return this->scoreSortingMethods;
+    }
 
     std::unordered_map<MD5Hash, std::vector<FinishedScore>> online_scores;
     static const std::string &getOsuSongsFolder();
@@ -102,7 +104,6 @@ class Database {
 
     UString parseLegacyCfgBeatmapDirectoryParameter();
     void scheduleLoadRaw();
-    void loadDB();
     std::mutex peppy_overrides_mtx;
     std::unordered_map<MD5Hash, MapOverrides> peppy_overrides;
     std::vector<BeatmapDifficulty *> maps_to_recalc;
@@ -112,14 +113,26 @@ class Database {
     std::mutex scores_mtx;
     std::unordered_map<MD5Hash, std::vector<FinishedScore>> scores;
 
+    // should only be accessed from database loader thread!
+    std::unordered_map<std::string, ByteBufferedFile::Reader> database_files;
+    u64 bytes_processed;
+    u64 total_bytes;
+    std::atomic<float> fLoadingProgress;
+
+    // fine to be modified as long as the db is not currently being loaded
+    std::vector<std::string> dbPathsToImport;
+
    private:
     friend class DatabaseLoader;
 
     void saveMaps();
 
-    void loadScores();
-    u32 importOldMcNeosuScores();
-    u32 importPeppyScores();
+    void openDatabases();
+    bool importDatabase(std::string db_path);
+    void loadMaps();
+    void loadScores(ByteBufferedFile::Reader &db);
+    void loadOldMcNeosuScores(ByteBufferedFile::Reader &db);
+    void loadPeppyScores(ByteBufferedFile::Reader &db);
     void saveScores();
     bool addScoreRaw(const FinishedScore &score);
     bool isScoreAlreadyInDB(u64 unix_timestamp, const MD5Hash &map_hash);
@@ -130,7 +143,6 @@ class Database {
 
     // global
     int iNumBeatmapsToLoad;
-    std::atomic<float> fLoadingProgress;
     std::atomic<bool> bInterruptLoad;
     std::vector<BeatmapSet *> beatmapsets;
     std::vector<BeatmapSet *> neosu_sets;

src/App/Osu/Osu.cpp
@@ -172,7 +172,6 @@ Osu::Osu() {
     this->fQuickSaveTime = 0.0f;
 
     this->bToggleModSelectionScheduled = false;
-    this->bToggleSongBrowserScheduled = false;
     this->bToggleOptionsMenuScheduled = false;
     this->bOptionsMenuFullscreen = true;
     this->bToggleChangelogScheduled = false;
@@ -363,9 +362,11 @@ Osu::Osu() {
 
 #ifdef _WIN32
     // Process cmdline args now, after everything has been initialized
+    std::vector<std::string> args;
     for(i32 i = 0; i < engine->iArgc; i++) {
-        mainloopPtrHack->handle_cmdline_args(engine->sArgv[i]);
+        args.push_back(engine->sArgv[i]);
     }
+    mainloopPtrHack->handle_cmdline_args(args);
 #endif
 
     // Not the type of shader you want players to tweak or delete, so loading from string
@@ -779,35 +780,6 @@ void Osu::update() {
         } else if(!this->isInPlayMode() && this->songBrowser2 != NULL) {
             this->songBrowser2->setVisible(!this->modSelector->isVisible());
         }
-    }
-    if(this->bToggleSongBrowserScheduled) {
-        this->bToggleSongBrowserScheduled = false;
-
-        if(this->mainMenu->isVisible() && this->optionsMenu->isVisible()) this->optionsMenu->setVisible(false);
-
-        if(this->songBrowser2 != NULL) this->songBrowser2->setVisible(!this->songBrowser2->isVisible());
-
-        if(bancho->is_in_a_multi_room()) {
-            // We didn't select a map; revert to previously selected one
-            auto diff2 = this->songBrowser2->lastSelectedBeatmap;
-            if(diff2 != NULL) {
-                bancho->room.map_name = UString::format("%s - %s [%s]", diff2->getArtist().c_str(),
-                                                        diff2->getTitle().c_str(), diff2->getDifficultyName().c_str());
-                bancho->room.map_md5 = diff2->getMD5Hash();
-                bancho->room.map_id = diff2->getID();
-
-                Packet packet;
-                packet.id = MATCH_CHANGE_SETTINGS;
-                bancho->room.pack(&packet);
-                BANCHO::Net::send_packet(packet);
-
-                this->room->on_map_change();
-            }
-        } else {
-            this->mainMenu->setVisible(!(this->songBrowser2 != NULL && this->songBrowser2->isVisible()));
-        }
-
-        this->updateConfineCursor();
     }
     if(this->bToggleOptionsMenuScheduled) {
         this->bToggleOptionsMenuScheduled = false;
@@ -1422,7 +1394,32 @@ void Osu::toggleModSelection(bool waitForF1KeyUp) {
 
 void Osu::toggleSongBrowser() {
     if(bancho->spectating) return;
-    this->bToggleSongBrowserScheduled = true;
+
+    if(this->mainMenu->isVisible() && this->optionsMenu->isVisible()) this->optionsMenu->setVisible(false);
+
+    this->songBrowser2->setVisible(!this->songBrowser2->isVisible());
+
+    if(bancho->is_in_a_multi_room()) {
+        // We didn't select a map; revert to previously selected one
+        auto diff2 = this->songBrowser2->lastSelectedBeatmap;
+        if(diff2 != NULL) {
+            bancho->room.map_name = UString::format("%s - %s [%s]", diff2->getArtist().c_str(),
+                                                    diff2->getTitle().c_str(), diff2->getDifficultyName().c_str());
+            bancho->room.map_md5 = diff2->getMD5Hash();
+            bancho->room.map_id = diff2->getID();
+
+            Packet packet;
+            packet.id = MATCH_CHANGE_SETTINGS;
+            bancho->room.pack(&packet);
+            BANCHO::Net::send_packet(packet);
+
+            this->room->on_map_change();
+        }
+    } else {
+        this->mainMenu->setVisible(!this->songBrowser2->isVisible());
+    }
+
+    this->updateConfineCursor();
 }
 
 void Osu::toggleOptionsMenu() {

commit 6e3b2e426d477899ef736cfdc58e3d627dcfbc21

fix missing replays for (stable)scores.db->(mcosu)scores.db->neosu_scores.db
regression from 81bc8355eeff0fa7c054e959a0e42508493d3827, due to always checking isScoreAlreadyInDB (mcosu scores are imported before peppy scores)

also, moved some score recalc logic to the ScoreConverterThread instead of having a separate scores_to_convert vector in Database, to make the score replacement/overwriting logic for simpler
the logic in ScoreConverterThread.cpp:score_needs_recalc might not be 100% correct, i have ~400 scores (out of 55000) which always require calc, don't know how to avoid that easily atm

other changes like for useMods and such are superficial, was trying to find the source of a semi-related issue which led to this

src/App/Osu/Database.cpp
@@ -97,7 +97,7 @@ void Database::AsyncDBLoader::init() {
     } else {
         MapCalcThread::start_calc(db->maps_to_recalc);
         VolNormalization::start_calc(db->loudness_to_calc);
-        sct_calc(db->scores_to_convert);
+        sct_calc(db->scores);
     }
 
     // signal that we are done
@@ -119,7 +119,6 @@ void Database::AsyncDBLoader::initAsync() {
     MapCalcThread::abort();
     VolNormalization::abort();
 
-    db->scores_to_convert.clear();
     db->loudness_to_calc.clear();
     db->maps_to_recalc.clear();
 
@@ -133,7 +132,7 @@ void Database::AsyncDBLoader::initAsync() {
     }
     db->neosu_sets.clear();
 
-    db->openDatabases();
+    db->findDatabases();
     db->loadScores(db->database_files["neosu_scores.db"]);
     db->loadOldMcNeosuScores(db->database_files["scores.db"]);
 
@@ -286,7 +285,7 @@ void Database::update() {
 
                 MapCalcThread::start_calc(this->maps_to_recalc);
                 VolNormalization::start_calc(this->loudness_to_calc);
-                sct_calc(this->scores_to_convert);
+                sct_calc(this->scores);
 
                 break;
             }
@@ -377,28 +376,56 @@ int Database::addScore(const FinishedScore &score) {
     return -1;
 }
 
-bool Database::isScoreAlreadyInDB(u64 unix_timestamp, const MD5Hash &map_hash) {
+int Database::isScoreAlreadyInDB(u64 unix_timestamp, const MD5Hash &map_hash) {
     std::scoped_lock lock(this->scores_mtx);
-    for(const auto &other : this->scores[map_hash]) {
-        if(other.unixTimestamp == unix_timestamp) {
+
+    for(int existing_pos = -1; const auto &existing : this->scores[map_hash]) {
+        existing_pos++;
+        if(existing.unixTimestamp == unix_timestamp) {
             // Score has already been added
-            return true;
+            return existing_pos;
         }
     }
 
-    return false;
+    return -1;
 }
 
 bool Database::addScoreRaw(const FinishedScore &score) {
-    if(this->isScoreAlreadyInDB(score.unixTimestamp, score.beatmap_hash)) {
-        return false;
+    const bool new_might_have_replay{score.has_possible_replay()};
+
+    int existing_pos{-1};
+    bool overwrite{false};
+
+    if((existing_pos = this->isScoreAlreadyInDB(score.unixTimestamp, score.beatmap_hash)) >= 0) {
+        // a bit hacky, but allow overwriting mcosu scores with peppy/neosu scores
+        // otherwise scores imported to mcosu from stable will be marked as "from mcosu"
+        // which we consider to never have a replay available
+
+        // we don't want to overwrite in any case if the new score has no possible replay
+        if(!new_might_have_replay) {
+            return false;
+        }
+
+        {
+            std::scoped_lock lock(this->scores_mtx);
+            // otherwise check if the old one doesn't have a replay
+            // if it has one, don't overwrite it
+            overwrite = !this->scores[score.beatmap_hash][existing_pos].has_possible_replay();
+        }
+
+        if(!overwrite) {
+            return false;
+        }
+        // otherwise overwrite it
     }
 
     std::scoped_lock lock(this->scores_mtx);
 
-    this->scores[score.beatmap_hash].push_back(score);
-    if(score.hitdeltas.empty()) {
-        this->scores_to_convert.push_back(score);
+    if(overwrite) {
+        this->scores[score.beatmap_hash][existing_pos] = score;
+    } else {
+        // new score
+        this->scores[score.beatmap_hash].push_back(score);
     }
 
     return true;
@@ -490,7 +517,7 @@ std::vector<UString> Database::getPlayerNamesWithScoresForUserSwitcher() {
     return names;
 }
 
-Database::PlayerPPScores Database::getPlayerPPScores(std::string playerName) {
+Database::PlayerPPScores Database::getPlayerPPScores(const std::string &playerName) {
     std::scoped_lock lock(this->scores_mtx);
     PlayerPPScores ppScores;
     ppScores.totalScore = 0;
@@ -544,7 +571,7 @@ Database::PlayerPPScores Database::getPlayerPPScores(std::string playerName) {
     return ppScores;
 }
 
-Database::PlayerStats Database::calculatePlayerStats(std::string playerName) {
+Database::PlayerStats Database::calculatePlayerStats(const std::string &playerName) {
     if(!this->bDidScoresChangeForStats.load() && playerName == this->prevPlayerStats.name.toUtf8())
         return this->prevPlayerStats;
 
@@ -599,7 +626,7 @@ Database::PlayerStats Database::calculatePlayerStats(std::string playerName) {
     return this->prevPlayerStats;
 }
 
-float Database::getWeightForIndex(int i) { return pow(0.95, (double)i); }
+float Database::getWeightForIndex(int i) { return std::pow(0.95f, (f32)i); }
 
 float Database::getBonusPPForNumScores(size_t numScores) {
     return (417.0 - 1.0 / 3.0) * (1.0 - pow(0.995, std::min(1000.0, (f64)numScores)));
@@ -857,10 +884,10 @@ void Database::loadMaps() {
                         diff->sArtistUnicode = diff->sArtist;
                     }
 
-                    if (SString::whitespace_only(diff->sTitleUnicode)) {
+                    if(SString::whitespace_only(diff->sTitleUnicode)) {
                         diff->bEmptyTitleUnicode = true;
                     }
-                    if (SString::whitespace_only(diff->sArtistUnicode)) {
+                    if(SString::whitespace_only(diff->sArtistUnicode)) {
                         diff->bEmptyArtistUnicode = true;
                     }
 
@@ -1069,7 +1096,7 @@ void Database::loadMaps() {
                     bpm.min = overrides->second.min_bpm;
                     bpm.max = overrides->second.max_bpm;
                     bpm.most_common = overrides->second.avg_bpm;
-                } else if (nb_timing_points > 0) {
+                } else if(nb_timing_points > 0) {
                     timing_points_buffer.resize(nb_timing_points);
                     if(db.read_bytes((u8 *)timing_points_buffer.data(),
                                      sizeof(Database::TIMINGPOINT) * nb_timing_points) !=
@@ -1158,7 +1185,7 @@ void Database::loadMaps() {
                 {
                     diff2->sTitle = songTitle;
                     diff2->sTitleUnicode = songTitleUnicode;
-                    if (SString::whitespace_only(diff2->sTitleUnicode)) {
+                    if(SString::whitespace_only(diff2->sTitleUnicode)) {
                         diff2->bEmptyTitleUnicode = true;
                     }
                     diff2->sAudioFileName = audioFileName;
@@ -1168,7 +1195,7 @@ void Database::loadMaps() {
 
                     diff2->sArtist = artistName;
                     diff2->sArtistUnicode = artistNameUnicode;
-                    if (SString::whitespace_only(diff2->sArtistUnicode)) {
+                    if(SString::whitespace_only(diff2->sArtistUnicode)) {
                         diff2->bEmptyArtistUnicode = true;
                     }
                     diff2->sCreator = creatorName;
@@ -1409,7 +1436,7 @@ void Database::saveMaps() {
     debugLog("Saved {:d} maps (+ {:d} overrides) in {:f} seconds.\n", nb_diffs_saved, nb_overrides, t.getElapsedTime());
 }
 
-void Database::openDatabases() {
+void Database::findDatabases() {
     this->bytes_processed = 0;
     this->total_bytes = 0;
     this->database_files.clear();
@@ -1774,7 +1801,7 @@ void Database::loadOldMcNeosuScores(const UString &dbPath) {
                     /* isImportedLegacyScore = */ db.skip<uint8_t>();  // too lazy to handle this logic
                 }
                 const auto unixTimestamp = db.read<uint64_t>();
-                if(this->isScoreAlreadyInDB(unixTimestamp, md5hash)) {
+                if(this->isScoreAlreadyInDB(unixTimestamp, md5hash) >= 0) {
                     db.skip_string();  // playerName
                     u32 bytesToSkipUntilNextScore = 0;
                     bytesToSkipUntilNextScore +=

src/App/Osu/Database.h
@@ -77,8 +77,8 @@ class Database {
 
     std::vector<UString> getPlayerNamesWithPPScores();
     std::vector<UString> getPlayerNamesWithScoresForUserSwitcher();
-    PlayerPPScores getPlayerPPScores(std::string playerName);
-    PlayerStats calculatePlayerStats(std::string playerName);
+    PlayerPPScores getPlayerPPScores(const std::string &playerName);
+    PlayerStats calculatePlayerStats(const std::string &playerName);
     static float getWeightForIndex(int i);
     static float getBonusPPForNumScores(size_t numScores);
     static unsigned long long getRequiredScoreForLevel(int level);
@@ -114,7 +114,6 @@ class Database {
     std::unordered_map<MD5Hash, MapOverrides> peppy_overrides;
     std::vector<BeatmapDifficulty *> maps_to_recalc;
     std::vector<BeatmapDifficulty *> loudness_to_calc;
-    std::vector<FinishedScore> scores_to_convert;
 
     std::mutex scores_mtx;
     std::atomic<bool> bDidScoresChangeForStats = true;
@@ -158,7 +157,7 @@ class Database {
 
     void saveMaps();
 
-    void openDatabases();
+    void findDatabases();
     bool importDatabase(const std::string &db_path);
     void loadMaps();
     void loadScores(const UString &dbPath);
@@ -166,7 +165,8 @@ class Database {
     void loadPeppyScores(const UString &dbPath);
     void saveScores();
     bool addScoreRaw(const FinishedScore &score);
-    bool isScoreAlreadyInDB(u64 unix_timestamp, const MD5Hash &map_hash);
+    // returns position of existing score in the scores[hash] array if found, -1 otherwise
+    int isScoreAlreadyInDB(u64 unix_timestamp, const MD5Hash &map_hash);
 
     AsyncDBLoader *loader{nullptr};
     Timing::Timer *importTimer;

src/App/Osu/Osu.cpp
@@ -839,8 +839,6 @@ bool Osu::isInPlayModeAndNotPaused() {
     return isInPlayMode() && !(getSelectedBeatmap() != nullptr && getSelectedBeatmap()->isPaused());
 }
 
-void Osu::useMods(FinishedScore *score) { Replay::Mods::use(score->mods); }
-
 void Osu::updateMods() {
     using namespace ModMasks;
     if(BanchoState::is_in_a_multi_room()) {

src/App/Osu/Osu.h
@@ -161,7 +161,8 @@ class Osu final : public MouseListener, public KeyboardListener {
     bool shouldFallBackToLegacySliderRenderer();  // certain mods or actions require Sliders to render dynamically
                                                   // (e.g. wobble or the CS override slider)
 
-    void useMods(FinishedScore *score);
+    inline void useMods(FinishedScore *score) {Replay::Mods::use(score->mods);}
+
     void updateMods();
     void updateCursorVisibility();
     void updateConfineCursor();

src/App/Osu/Replay.h
@@ -1,66 +1,69 @@
 #pragma once
 // Copyright (c) 2016, PG, All rights reserved.
-#include "LegacyReplay.h"
+#include "types.h"
 
 class DatabaseBeatmap;
 
 namespace Replay {
 
 namespace ModFlags {
+enum ModFlags_ : u64 {
 
 // Green mods
-const u64 NoFail = 1ULL << 0;
-const u64 Easy = 1ULL << 1;
-const u64 Autopilot = 1ULL << 2;
-const u64 Relax = 1ULL << 3;
+NoFail = 1ULL << 0,
+Easy = 1ULL << 1,
+Autopilot = 1ULL << 2,
+Relax = 1ULL << 3,
 
 // Red mods
-const u64 Hidden = 1ULL << 4;
-const u64 HardRock = 1ULL << 5;
-const u64 Flashlight = 1ULL << 6;
-const u64 SuddenDeath = 1ULL << 7;
-const u64 Perfect = SuddenDeath | (1ULL << 8);
-const u64 Nightmare = 1ULL << 9;
+Hidden = 1ULL << 4,
+HardRock = 1ULL << 5,
+Flashlight = 1ULL << 6,
+SuddenDeath = 1ULL << 7,
+Perfect = SuddenDeath | (1ULL << 8),
+Nightmare = 1ULL << 9,
 
 // Special mods
-const u64 NoPitchCorrection = 1ULL << 10;
-const u64 TouchDevice = 1ULL << 11;
-const u64 SpunOut = 1ULL << 12;
-const u64 ScoreV2 = 1ULL << 13;
-const u64 FPoSu = 1ULL << 14;
-const u64 Target = 1ULL << 15;
+NoPitchCorrection = 1ULL << 10,
+TouchDevice = 1ULL << 11,
+SpunOut = 1ULL << 12,
+ScoreV2 = 1ULL << 13,
+FPoSu = 1ULL << 14,
+Target = 1ULL << 15,
 
 // Experimental mods
-const u64 AROverrideLock = 1ULL << 16;
-const u64 ODOverrideLock = 1ULL << 17;
-const u64 Timewarp = 1ULL << 18;
-const u64 ARTimewarp = 1ULL << 19;
-const u64 Minimize = 1ULL << 20;
-const u64 Jigsaw1 = 1ULL << 21;
-const u64 Jigsaw2 = 1ULL << 22;
-const u64 Wobble1 = 1ULL << 23;
-const u64 Wobble2 = 1ULL << 24;
-const u64 ARWobble = 1ULL << 25;
-const u64 FullAlternate = 1ULL << 26;
-const u64 Shirone = 1ULL << 27;
-const u64 Mafham = 1ULL << 28;
-const u64 HalfWindow = 1ULL << 29;
-const u64 HalfWindowAllow300s = 1ULL << 30;
-const u64 Ming3012 = 1ULL << 31;
-const u64 No100s = 1ULL << 32;
-const u64 No50s = 1ULL << 33;
-const u64 MirrorHorizontal = 1ULL << 34;
-const u64 MirrorVertical = 1ULL << 35;
-const u64 FPoSu_Strafing = 1ULL << 36;
-const u64 FadingCursor = 1ULL << 37;
-const u64 FPS = 1ULL << 38;
-const u64 ReverseSliders = 1ULL << 39;
-const u64 Millhioref = 1ULL << 40;
-const u64 StrictTracking = 1ULL << 41;
-const u64 ApproachDifferent = 1ULL << 42;
+AROverrideLock = 1ULL << 16,
+ODOverrideLock = 1ULL << 17,
+Timewarp = 1ULL << 18,
+ARTimewarp = 1ULL << 19,
+Minimize = 1ULL << 20,
+Jigsaw1 = 1ULL << 21,
+Jigsaw2 = 1ULL << 22,
+Wobble1 = 1ULL << 23,
+Wobble2 = 1ULL << 24,
+ARWobble = 1ULL << 25,
+FullAlternate = 1ULL << 26,
+Shirone = 1ULL << 27,
+Mafham = 1ULL << 28,
+HalfWindow = 1ULL << 29,
+HalfWindowAllow300s = 1ULL << 30,
+Ming3012 = 1ULL << 31,
+No100s = 1ULL << 32,
+No50s = 1ULL << 33,
+MirrorHorizontal = 1ULL << 34,
+MirrorVertical = 1ULL << 35,
+FPoSu_Strafing = 1ULL << 36,
+FadingCursor = 1ULL << 37,
+FPS = 1ULL << 38,
+ReverseSliders = 1ULL << 39,
+Millhioref = 1ULL << 40,
+StrictTracking = 1ULL << 41,
+ApproachDifferent = 1ULL << 42,
 
 // Non-submittable
-const u64 Autoplay = 1ULL << 63;
+Autoplay = 1ULL << 63
+
+};
 
 }  // namespace ModFlags
 

src/App/Osu/SongBrowser/ScoreButton.cpp
@@ -38,22 +38,8 @@
 UString ScoreButton::recentScoreIconString;
 
 ScoreButton::ScoreButton(UIContextMenu *contextMenu, float xPos, float yPos, float xSize, float ySize, STYLE style)
-    : CBaseUIButton(xPos, yPos, xSize, ySize, "", "") {
-    this->contextMenu = contextMenu;
-    this->style = style;
-
+    : CBaseUIButton(xPos, yPos, xSize, ySize, "", ""), contextMenu(contextMenu), style(style) {
     if(recentScoreIconString.length() < 1) recentScoreIconString.insert(0, Icons::ARROW_CIRCLE_UP);
-
-    this->fIndexNumberAnim = 0.0f;
-    this->bIsPulseAnim = false;
-
-    this->bRightClick = false;
-    this->bRightClickCheck = false;
-
-    this->iScoreIndexNumber = 1;
-    this->iScoreUnixTimestamp = 0;
-
-    this->scoreGrade = FinishedScore::Grade::D;
 }
 
 ScoreButton::~ScoreButton() {
@@ -580,6 +566,7 @@ void ScoreButton::onRightMouseUpInside() {
                 replayButton->setEnabled(false);
                 replayButton->setTextColor(0xff888888);
                 replayButton->setTextDarkColor(0xff000000);
+                // debugLog("disallowing replay button, client: {}\n", this->score.client);
             }
 
             CBaseUIButton *spacer = this->contextMenu->addButton("---");
@@ -835,23 +822,30 @@ void ScoreButton::setScore(const FinishedScore &score, DatabaseBeatmap *diff2, i
 bool ScoreButton::isContextMenuVisible() { return (this->contextMenu != nullptr && this->contextMenu->isVisible()); }
 
 SkinImage *ScoreButton::getGradeImage(FinishedScore::Grade grade) {
+    const auto &skin{osu->getSkin()};
+    if(!skin) {
+        debugLog("no skin to return a grade image for\n");
+        return nullptr;
+    }
+
+    using enum FinishedScore::Grade;
     switch(grade) {
-        case FinishedScore::Grade::XH:
-            return osu->getSkin()->getRankingXHsmall();
-        case FinishedScore::Grade::SH:
-            return osu->getSkin()->getRankingSHsmall();
-        case FinishedScore::Grade::X:
-            return osu->getSkin()->getRankingXsmall();
-        case FinishedScore::Grade::S:
-            return osu->getSkin()->getRankingSsmall();
-        case FinishedScore::Grade::A:
-            return osu->getSkin()->getRankingAsmall();
-        case FinishedScore::Grade::B:
-            return osu->getSkin()->getRankingBsmall();
-        case FinishedScore::Grade::C:
-            return osu->getSkin()->getRankingCsmall();
+        case XH:
+            return skin->getRankingXHsmall();
+        case SH:
+            return skin->getRankingSHsmall();
+        case X:
+            return skin->getRankingXsmall();
+        case S:
+            return skin->getRankingSsmall();
+        case A:
+            return skin->getRankingAsmall();
+        case B:
+            return skin->getRankingBsmall();
+        case C:
+            return skin->getRankingCsmall();
         default:
-            return osu->getSkin()->getRankingDsmall();
+            return skin->getRankingDsmall();
     }
 }
 

src/App/Osu/SongBrowser/ScoreButton.h
@@ -14,7 +14,7 @@ class ScoreButton : public CBaseUIButton {
     static SkinImage *getGradeImage(FinishedScore::Grade grade);
     static UString getModsStringForDisplay(Replay::Mods mods);
 
-    enum class STYLE { SONG_BROWSER, TOP_RANKS };
+    enum class STYLE : uint8_t { SONG_BROWSER, TOP_RANKS };
 
     ScoreButton(UIContextMenu *contextMenu, float xPos, float yPos, float xSize, float ySize,
                 STYLE style = STYLE::SONG_BROWSER);
@@ -62,20 +62,6 @@ class ScoreButton : public CBaseUIButton {
     bool isContextMenuVisible();
 
     UIContextMenu *contextMenu;
-    STYLE style;
-    float fIndexNumberAnim;
-    bool bIsPulseAnim;
-
-    bool bRightClick;
-    bool bRightClickCheck;
-
-    // score data
-    FinishedScore score;
-
-    int iScoreIndexNumber;
-    u64 iScoreUnixTimestamp;
-
-    FinishedScore::Grade scoreGrade;
 
     // STYLE::SCORE_BROWSER
     UString sScoreTime;
@@ -95,4 +81,19 @@ class ScoreButton : public CBaseUIButton {
 
     std::vector<UString> tooltipLines;
     UString sScoreDateTime;
+
+    // score data
+    FinishedScore score;
+
+    int iScoreIndexNumber{1};
+    u64 iScoreUnixTimestamp{0};
+
+    FinishedScore::Grade scoreGrade{FinishedScore::Grade::D};
+
+    STYLE style;
+    float fIndexNumberAnim{0.0f};
+    bool bIsPulseAnim{false};
+
+    bool bRightClick{false};
+    bool bRightClickCheck{false};
 };

src/App/Osu/SongBrowser/ScoreConverterThread.cpp
@@ -8,12 +8,14 @@
 #include "SimulatedBeatmap.h"
 #include "score.h"
 
+// TODO
+static constexpr bool USE_PPV3{false};
+
 std::atomic<u32> sct_computed = 0;
 std::atomic<u32> sct_total = 0;
 
 static std::thread thr;
 static std::atomic<bool> dead = true;
-static std::vector<FinishedScore> scores;
 
 static std::vector<f64> aimStrains;
 static std::vector<f64> speedStrains;
@@ -91,11 +93,41 @@ static void update_ppv2(const FinishedScore& score) {
     db->scores_mtx.unlock();
 }
 
-static void run_sct() {
+static forceinline bool score_needs_recalc(const FinishedScore& score) {
+    if((USE_PPV3 && score.hitdeltas.empty())
+       // should this be < or != ... ?
+       || (score.ppv2_version < DifficultyCalculator::PP_ALGORITHM_VERSION)
+       // is this correct? e.g. if we can never successfully calculate for a score, what to do? we just keep trying and failing
+       || (score.ppv2_score <= 0.f)) {
+        return true;
+    }
+    return false;
+}
+
+static void run_sct(const std::unordered_map<MD5Hash, std::vector<FinishedScore>> &all_set_scores) {
     debugLog("Started score converter thread\n");
 
-    i32 idx = 0;
-    for(auto& score : scores) {
+    // defer the actual needs-recalc check to run on the thread, to avoid unnecessarily blocking (O(n^2) loop)
+    std::vector<FinishedScore> scores_to_calc;
+    // lazy reserve, assume 3 scores per score vector
+    scores_to_calc.reserve(all_set_scores.size() * 3);
+
+    for(const auto& [_, beatmap] : all_set_scores) {
+        for(const auto& score : beatmap) {
+            if(score_needs_recalc(score)) {
+                scores_to_calc.push_back(score);
+            }
+        }
+    }
+
+    sct_total = scores_to_calc.size();
+
+    debugLog("Found {} scores which need pp recalculation\n", sct_total.load());
+
+    // nothing to do...
+    if(sct_total == 0) return;
+
+    for(i32 idx = 0; auto& score : scores_to_calc) {
         while(osu->should_pause_background_threads.load() && !dead.load()) {
             Timing::sleepMS(100);
         }
@@ -106,12 +138,14 @@ static void run_sct() {
         // This is "placeholder" until we get accurate replay simulation
         {
             update_ppv2(score);
-            sct_computed++;
-            idx++;
         }
 
         // @PPV3: below
-        continue;
+        if(!USE_PPV3) {
+            sct_computed++;
+            idx++;
+            continue;
+        }
 
         if(score.replay.empty()) {
             if(!LegacyReplay::load_from_disk(&score, false)) {
@@ -142,10 +176,10 @@ static void run_sct() {
                      smap.live_score.getNumMisses());
 
         db->scores_mtx.lock();
-        for(auto& other : (*db->getScores())[score.beatmap_hash]) {
-            if(other.unixTimestamp == score.unixTimestamp) {
+        for(auto& dbScore : (*db->getScores())[score.beatmap_hash]) {
+            if(dbScore.unixTimestamp == score.unixTimestamp) {
                 // @PPV3: currently hitdeltas is always empty
-                other.hitdeltas = score.hitdeltas;
+                dbScore.hitdeltas = score.hitdeltas;
                 break;
             }
         }
@@ -160,15 +194,18 @@ static void run_sct() {
     sct_computed++;
 }
 
-void sct_calc(std::vector<FinishedScore> scores_to_calc) {
+void sct_calc(std::unordered_map<MD5Hash, std::vector<FinishedScore>> scores_to_maybe_calc) {
     sct_abort();
-    if(scores_to_calc.empty()) return;
 
     dead = false;
-    scores = std::move(scores_to_calc);
+
+    // to be set in run_sct (find scores which actually need recalc)
+    sct_total = 0;
     sct_computed = 0;
-    sct_total = scores.size() + 1;
-    thr = std::thread(run_sct);
+
+    if (!scores_to_maybe_calc.empty()) {
+        thr = std::thread(run_sct, std::move(scores_to_maybe_calc));
+    }
 }
 
 void sct_abort() {

src/App/Osu/SongBrowser/ScoreConverterThread.h
@@ -1,15 +1,17 @@
 #pragma once
 // Copyright (c) 2024, kiwec, All rights reserved.
 
-#include <atomic>
-#include <mutex>
+#include "types.h"
+#include "MD5Hash.h"
 
-#include "Database.h"
+#include <atomic>
+#include <unordered_map>
 
 class DatabaseBeatmap;
+struct FinishedScore;
 
 extern std::atomic<u32> sct_computed;
 extern std::atomic<u32> sct_total;
 
-void sct_calc(std::vector<FinishedScore> scores_to_calc);
+void sct_calc(std::unordered_map<MD5Hash, std::vector<FinishedScore>> scores_to_maybe_calc);
 void sct_abort();

src/App/Osu/score.h
@@ -1,11 +1,15 @@
 #pragma once
-#include "Replay.h"
 #include "MD5Hash.h"
+#include "Replay.h"
 
 class ConVar;
 class DatabaseBeatmap;
 class BeatmapInterface;
 class HitObject;
+
+namespace LegacyReplay {
+    struct Frame;
+}
 
 struct FinishedScore {
     u64 score = 0;